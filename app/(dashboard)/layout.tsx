2acfddc674c37024b9118cfc94847c16c1d822f5
Dajana Smolyakov <dajana@zyax.se>
2023-08-25 16:48:02 +0200

    Add menus

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,5 +1,26 @@
+import { UserButton } from "@clerk/nextjs";
+import styles from "./layout.module.scss";
+
 const DashboardLayout = ({ children }) => {
-  return <div>{children}</div>;
+  return (
+    <div className={styles.dashboard}>
+      <aside className={styles.sideMenu}>Journey</aside>
+      <div className={styles.content}>
+        <header>
+          <nav>
+            <ul className={styles.navBar}>
+              <li>Journal</li>
+              <li>Insights</li>
+              <li className={styles.user}>
+                <UserButton />
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <main>{children}</main>
+      </div>
+    </div>
+  );
 };
 
 export default DashboardLayout;
